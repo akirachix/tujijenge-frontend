177e605326b5563ece6aa1b5f797cc7c06a18812
A <Arsema.arsemaagebremichael@gmail.com>
2025-07-03 06:18:29 +0300

    Edit button and add differenct versions

---
+++ tujijenge/src/components/SharedComponets/Button/button.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Button from './button';
+
+test('renders the button', () => {
+  render(<Button label="Click me" />);
+  expect(screen.getByRole('button', { name: 'Click me' })).toBeInTheDocument();
+});
---
+++ tujijenge/src/components/SharedComponets/Form/form.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './form.css'
+
+
+function Input({ id, label, value, onChange, type = "text", placeholder }) {
+  return (
+    <div className="form-group">
+      <label htmlFor={id}>{label}</label>
+      <input 
+        id={id}
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="form-input"
+      />
+    </div>
+  );
+}
+
+export default Input;
