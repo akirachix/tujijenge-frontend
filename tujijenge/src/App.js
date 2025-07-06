288eeac5b875ecae6089559e9da3c65c3191e693
Arsema <arsemaagebremichael@gmail.com>
2025-07-06 16:06:31 +0300

    Remove components from Boiler plate

--- tujijenge/src/App.js
+++ tujijenge/src/App.js
@@ -1,46 +1,9 @@
 import './App.css';
-import Button from './components/SharedComponets/Button/button'
-import Input from './components/SharedComponets/Form/form'
-import React, { useState } from "react";
+
 
 
 function App() {
-  const [email, setEmail] = useState("");
-  return (
-    <div className="App">
 
-      <Button
-        label="Save"
-        onClick={() => alert('Saved!')}
-        type="submit"
-        disabled={false}
-      />
-
-      <Button
-        label="Cancel!"
-        variant="secondary"
-        onClick={() => alert('Canceled!')}
-      />
-
-
-      <Button
-        label="Cancel!"
-        variant="quaternary"
-        onClick={() => alert('Canceled!')}
-      />
-
-      
-<Input
-        id="email"
-        label="Email:"
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-        
-    </div>
-  );
 }
 
 
