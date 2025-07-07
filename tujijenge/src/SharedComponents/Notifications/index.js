089927a6f6ecae32e391329f7d08fe73cf097f1a
karen <karenwanngugi@gmail.com>
2025-07-07 16:58:16 +0300

    Added the notification component

--- tujijenge/src/SharedComponents/Notifications/index.js
+++ tujijenge/src/SharedComponents/Notifications/index.js
@@ -1,7 +1,7 @@
 import React, { useState,useRef,useEffect } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
-import './notification.css';
+import './index.css';
 
 
 
