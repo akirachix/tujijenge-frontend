da09842f4bb09e851c410c46df413c3b2a22d40d
karen <karenwanngugi@gmail.com>
2025-07-09 16:06:31 +0300

    Updated the sidebar and notifications component

--- tujijenge/src/App.js
+++ tujijenge/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
-import GroupOrders from './Components/GroupOrders';
-import CommunityOrders from './Components/CommunityOrders';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<CommunityOrders/>}/>
-        <Route path='/group-orders/:groupId' element={<GroupOrders/>}/>
+    <div>
+      
+    </div>
 
-      </Routes>
-    </Router>
   );
 }
 
