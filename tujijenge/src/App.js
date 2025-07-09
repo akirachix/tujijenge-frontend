dff6d0a53d4be7fd19b2688dea16deab02750b2b
Arsema <arsemaagebremichael@gmail.com>
2025-07-09 16:27:16 +0300

    Update App.js

--- tujijenge/src/App.js
+++ tujijenge/src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
-import Button from './SharedComponents/Button';
-import Input from './SharedComponents/Input';
 import React, { useState } from "react";
 
 function App() {
-  const handleClick = (action) => {
-    console.log('button clicked!')};
+
 
   return (
     <div >
-      <h1 style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '16px' }}>Tujijenge Buttons and input field</h1>
-      <Button label="Login" variant="primary" onClick={() => handleClick('Login')} />
-      <Button label="Join Community" variant="secondary" onClick={() => handleClick('Join')} />
-      <Button label="Submit" variant="tertiary" onClick={() => handleClick('Submit')} />
-      <Button label="Cancel" variant="quaternary" onClick={() => handleClick('Cancel')} />
-      <Input 
-        label={"Email:"}
-    placeholder ={"email"}
-    />  
-    
-    
+      <h1>Tujijenge Buttons and input field</h1>
        </div>
   );
 }
